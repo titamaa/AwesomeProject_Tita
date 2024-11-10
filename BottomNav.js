a2986b7781bb3051fccb8f5bd465f7380586b0e5
titamaa <titaamaliasudarma@mail.ugm.ac.id>
2024-11-10 22:14:16 +0700

    pgpbl10

--- BottomNav.js
+++ BottomNav.js
@@ -2,20 +2,30 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Profil from './App';
+import Mahasiswa from './Mahasiswa';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faUser, faUserGraduate} from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { WebView } from 'react-native-webview';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <Profil/>
   );
 }
 
-function SettingsScreen() {
+function DataMahasiswaScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
+    <Mahasiswa/>
+  );
+}
+
+function WebScreen() {
+  return (
+    <WebView
+        source={{ uri: 'https://github.com/titaama' }}
+      /> 
   );
 }
 
@@ -25,8 +35,23 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profil" component={HomeScreen} options={{ headerShown: false,
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faUser} color={color} size={20}/>
+        ),
+         }} />
+
+        <Tab.Screen name="Mahasiswa" component={DataMahasiswaScreen} options={{
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faUserGraduate} color={color} size={20}/>
+          ),
+        }} />
+        
+        <Tab.Screen name="GitHUB" component={WebScreen} options={{
+          tabBarIcon: ({color}) => (
+            <FontAwesomeIcon icon={faGithub} color={color} size={20}/>
+          ),
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
